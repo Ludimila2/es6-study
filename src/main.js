aca61d4972985aeceba56b9c91dc305971b02e95
Ludimila Oliveira <ludimillaoliveira9@gmail.com>
2020-12-04 10:30:02 -0300

    modulo sobre classes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
-alert('teste')
-
-class Teste {
-    metodo() {
-
+// CLASSES
+class TodoList {
+    constructor() {
+        this.todos = []
     }
 
-    outroMetodo() {
-        
+    // para adicionar novo to do
+    addTodo() {
+        this.todos.push('Novo to do')
+        console.log(this.todos)
     }
+}
+
+// para armazenar uma nova lista
+const MinhaLista = new TodoList()
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.addTodo()
 }
