7d69c4363be365cc87b365525d7d8edd24f32c4c
Ludimila Oliveira <ludimillaoliveira9@gmail.com>
2020-12-09 09:37:59 -0300

    operacoes em array

--- src/bundle.js
+++ src/bundle.js
@@ -1,34 +1,19 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var arr = [1, 2, 3, 4];
+console.log(arr); // soma o item e sua posição
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr); // retorna o total da adição mais o próximo item
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum); // retorna apenas os números pares
 
-// CLASSES
-var TodoList = /*#__PURE__*/function () {
-  function TodoList() {
-    _classCallCheck(this, TodoList);
-
-    this.todos = [];
-  } // para adicionar novo to do
-
-
-  _createClass(TodoList, [{
-    key: "addTodo",
-    value: function addTodo() {
-      this.todos.push('Novo to do');
-      console.log(this.todos);
-    }
-  }]);
-
-  return TodoList;
-}(); // para armazenar uma nova lista
-
-
-var MinhaLista = new TodoList();
-
-document.getElementById('novotodo').onclick = function () {
-  MinhaLista.addTodo();
-};
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
