a65210c3544e0132cce5346bfc0a37693127f50c
Ludimila Oliveira <ludimillaoliveira9@gmail.com>
2020-12-09 09:37:30 -0300

    operacoes em array

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
-// CLASSES
-class TodoList {
-    constructor() {
-        this.todos = []
-    }
+const arr = [1, 2, 3, 4];
+console.log(arr);
 
-    // para adicionar novo to do
-    addTodo() {
-        this.todos.push('Novo to do')
-        console.log(this.todos)
-    }
-}
+// soma o item e sua posição
+const newArr = arr.map(function(item, index) {
+    return item + index;
+})
 
-// para armazenar uma nova lista
-const MinhaLista = new TodoList()
+console.log(newArr);
 
-document.getElementById('novotodo').onclick = function() {
-    MinhaLista.addTodo()
-}
+// retorna o total da adição mais o próximo item
+const sum = arr.reduce(function(total, next) {
+    return total + next;
+})
+
+console.log(sum);
+
+// retorna apenas os números pares
+const filter = arr.filter(function(item) {
+    return item % 2 === 0;
+})
+
+console.log(filter);
